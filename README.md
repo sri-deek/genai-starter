ab3fd8f17a104e0176b97bd654e56e94b6a74686
ML expert <securemlops@gmail.com>
2025-06-19 17:57:03 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # genai-starter
 First code to start with genai
+
+
+### setup
+- install packages from requirements.txt
+    - pip install -r requirements.txt
+    
---
+++ src/genai_start/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GEMINI_ID_KEY = os.getenv("GEMINI_ID_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+def set_environment():
+    variable_dict = globals().items()
+    for key, value in variable_dict:
+        if "API" in key or "ID" in key:
+            if value is not None:
+                os.environ[key] = value
